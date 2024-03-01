664f84105d83ecd4b7983ba2d6b8ba9379d354fe
Kerciu <kerciuuu@gmail.com>
2024-03-01 20:50:53 +0100

    Dodaj pliki projektów.

---
+++ dateTest/dateTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace dateTest
+{
+	TEST_CLASS(dateTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
