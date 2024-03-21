a069f0bdc74d619b583748b119f15afbd0035020
kgorski1 <01187228@pw.edu.pl>
2024-03-21 09:50:31 +0100

    added test file

---
+++ Zadanie2/Szybki_Lopez/fastSpoilingLib/fastSpoilingTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace dateTest
+{
+	TEST_CLASS(dateTest)
+	{
+	public:
+		
+		TEST_METHOD()
+		{
+			
+		}
+    }
+};
