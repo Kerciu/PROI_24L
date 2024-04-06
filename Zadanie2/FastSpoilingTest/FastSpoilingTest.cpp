95464416565e76e20bd443526017d0955dbeb6f2
Kerciu <kerciuuu@gmail.com>
2024-04-06 15:06:26 +0200

    Added tests, some of them passed (failed)

--- Zadanie2/FastSpoilingTest/FastSpoilingTest.cpp
+++ Zadanie2/FastSpoilingTest/FastSpoilingTest.cpp
@@ -1,5 +1,5 @@
-#include "pch.h"
 #include "CppUnitTest.h"
+#include <sstream>
 #include "../FastSpoiling/fastSpoiling.h"
 #include "../FastSpoiling/Transport.h"
 #include "../FastSpoiling/weightAndVolume.h"
@@ -18,8 +18,8 @@ namespace FastSpoilingTest
 			// Arrange
 			std::string itemName = "Apple";
 			std::string itemType = "Fruit";
-			Date productionDate(2023, 3, 15);
-			Date expirationDate(2023, 3, 20);
+			Date productionDate(15, 3, 2024);
+			Date expirationDate(20, 3, 2024);
 			Transport transport(15.5, "In a box", 50);
 			weightAndVolume weightVolume(0.2, 0.1);
 
@@ -38,13 +38,13 @@ namespace FastSpoilingTest
 		TEST_METHOD(SetFastSpoilingAttributes)
 		{
 			// Arrange
-			fastSpoiling fs("Apple", "Fruit", Date(2023, 3, 15), Date(2023, 3, 20), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
+			fastSpoiling fs("Apple", "Fruit", Date(15, 3, 2023), Date(20, 3, 2023), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
 
 			// Act
 			fs.setName("Banana");
 			fs.setType("Fruit");
-			fs.setProductionDate(2023, 3, 16);
-			fs.setExpirationDate(2023, 3, 21);
+			fs.setProductionDate(16, 3, 2023);
+			fs.setExpirationDate(21, 3, 2023);
 			fs.setTransportTemperature(20.0);
 			fs.setTransportPackaging("Crate");
 			fs.setWeight(0.3);
@@ -53,8 +53,8 @@ namespace FastSpoilingTest
 			// Assert
 			Assert::AreEqual("Banana", fs.getName().c_str());
 			Assert::AreEqual("Fruit", fs.getType().c_str());
-			Assert::AreEqual(Date(2023, 3, 16).slashOutput(), fs.getProductionDateSlashed());
-			Assert::AreEqual(Date(2023, 3, 21).slashOutput(), fs.getExpirationDateSlashed());
+			Assert::AreEqual(Date(16, 3, 2023).slashOutput(), fs.getProductionDateSlashed());
+			Assert::AreEqual(Date(21, 3, 2023).slashOutput(), fs.getExpirationDateSlashed());
 			Assert::AreEqual(0.3, fs.getWeight());
 			Assert::AreEqual(0.2, fs.getVolume());
 		}
@@ -62,9 +62,9 @@ namespace FastSpoilingTest
 		TEST_METHOD(FastSpoilingEqualityOperator)
 		{
 			// Arrange
-			fastSpoiling fs1("Apple", "Fruit", Date(2023, 3, 15), Date(2023, 3, 20), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
-			fastSpoiling fs2("Apple", "Fruit", Date(2023, 3, 15), Date(2023, 3, 20), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
-			fastSpoiling fs3("Banana", "Fruit", Date(2023, 3, 16), Date(2023, 3, 21), Transport(15.5, "In a cooler", 50), weightAndVolume(0.3, 0.2));
+			fastSpoiling fs1("Apple", "Fruit", Date(15, 3, 2023), Date(15, 3, 2023), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
+			fastSpoiling fs2("Apple", "Fruit", Date(15, 3, 2023), Date(15, 3, 2023), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
+			fastSpoiling fs3("Banana", "Fruit", Date(14, 3, 2023), Date(16, 3, 2023), Transport(15.5, "In a cooler", 50), weightAndVolume(0.3, 0.2));
 
 			// Act & Assert
 			Assert::IsTrue(fs1 == fs2);
@@ -140,25 +140,35 @@ namespace FastSpoilingTest
 		TEST_METHOD(WeightConversion)
 		{
 			// Arrange
-			weightAndVolume wv(2500, 3.0); // 2500 gramów = 2.5 kg
+			weightAndVolume wv(2500, 3.0); // 2500 kilo
 
 			// Act
-			double weightInKilograms = wv.getWeight("kilogram");
+			double weightInTonnes = wv.getWeight("tonne");
 
 			// Assert
-			Assert::AreEqual(2.5, weightInKilograms);
+			if (weightInTonnes != -1) {
+				Assert::AreEqual(2.5, weightInTonnes, 0.001);		// Precyzja
+			}
+			else {
+				Assert::Fail(L"Invalid unit");
+			}
 		}
 
 		TEST_METHOD(VolumeConversion)
 		{
 			// Arrange
-			weightAndVolume wv(2.5, 3000); // 3000 mililitrów = 3 litry
+			weightAndVolume wv(3000, 3000); // 3000 litrów
 
 			// Act
-			double volumeInLiters = wv.getVolume("liter");
+			double volumeInLiters = wv.getVolume("hectoliter");
 
 			// Assert
-			Assert::AreEqual(3.0, volumeInLiters);
+			if (volumeInLiters != -1) {
+				Assert::AreEqual(3.0, volumeInLiters, 0.001);
+			}
+			else {
+				Assert::Fail(L"Invalid unit");
+			}
 		}
 
 		TEST_METHOD(WeightAndVolumeAdditionOperator)
@@ -217,21 +227,18 @@ namespace FastSpoilingTest
 
 		TEST_METHOD(AddElement)
 		{
-			// Arrange
-			fastSpoiling fs("Apple", "Fruit", Date(2023, 3, 15), Date(2023, 3, 20), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
+			fastSpoiling fs("Apple", "Fruit", Date(15, 3, 2023), Date(20, 3, 2023), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
 			fastSpoilingCollection collection;
 
-			// Act
 			collection.addNewElement(fs);
 
-			// Assert
 			Assert::AreEqual(static_cast<size_t>(1), collection.sizeOfCollection());
 		}
 
 		TEST_METHOD(SearchElement)
 		{
 			// Arrange
-			fastSpoiling fs("Apple", "Fruit", Date(2023, 3, 15), Date(2023, 3, 20), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
+			fastSpoiling fs("Apple", "Fruit", Date(15, 3, 2023), Date(20, 3, 2023), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
 			fastSpoilingCollection collection;
 			collection.addNewElement(fs);
 
@@ -244,7 +251,7 @@ namespace FastSpoilingTest
 		TEST_METHOD(DeleteElement)
 		{
 			// Arrange
-			fastSpoiling fs("Apple", "Fruit", Date(2023, 3, 15), Date(2023, 3, 20), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
+			fastSpoiling fs("Apple", "Fruit", Date(15, 3, 2023), Date(20, 3, 2023), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
 			fastSpoilingCollection collection;
 			collection.addNewElement(fs);
 
@@ -258,23 +265,124 @@ namespace FastSpoilingTest
 		TEST_METHOD(ModifyElement)
 		{
 			// Arrange
-			fastSpoiling fs("Apple", "Fruit", Date(2023, 3, 15), Date(2023, 3, 20), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
+			fastSpoiling fs("Apple", "Fruit", Date(15, 3, 2023), Date(20, 3, 2023), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
 			fastSpoilingCollection collection;
 			collection.addNewElement(fs);
 
 			// Act
-			collection.modifyCollectionElement(1, "Banana", "Fruit", Date(2023, 3, 16), Date(2023, 3, 21), Transport(20.0, "In a cooler", 60), weightAndVolume(0.3, 0.2));
+			collection.modifyCollectionElement(0, "Banana", "Fruit", Date(16, 3, 2023), Date(21, 3, 2024), Transport(20.0, "In a cooler", 60), weightAndVolume(0.3, 0.2));
 
 			// Assert
 			Assert::AreEqual("Banana", fs.getName().c_str());
 			Assert::AreEqual("Fruit", fs.getType().c_str());
-			Assert::AreEqual(Date(2023, 3, 16).slashOutput(), fs.getProductionDateSlashed());
-			Assert::AreEqual(Date(2023, 3, 21).slashOutput(), fs.getExpirationDateSlashed());
+			Assert::AreEqual(Date(16, 3, 2023).slashOutput(), fs.getProductionDateSlashed());
+			Assert::AreEqual(Date(21, 3, 2024).slashOutput(), fs.getExpirationDateSlashed());
 			Assert::AreEqual(0.3, fs.getWeight());
 			Assert::AreEqual(0.2, fs.getVolume());
 			Assert::AreEqual(20.0, fs.getTransportTemperature());
 			Assert::AreEqual("In a cooler", fs.getTransportPackaging().c_str());
 			Assert::AreEqual(60.0, fs.getTransportHumidity());
+		}
+
+		TEST_METHOD(RepresentCollection) {
+			fastSpoiling fs("Apple", "Fruit", Date(15, 3, 2023), Date(20, 3, 2023), Transport(15.5, "In a box", 50), weightAndVolume(0.2, 0.1));
+			std::ostringstream oss;
+			oss << fs;
+			Assert::AreEqual(oss.str().c_str(), "Apple;Fruit;15/03/2023;20/03/2023;15.5;In a box;50;0.2;0.1\n");
+		}
+
+		TEST_METHOD(TransportOutputOperator)
+		{
+			Transport transport(25.5, "In a box", 50);
+			std::ostringstream oss;
+			oss << transport;
+			Assert::AreEqual(oss.str().c_str(), "25.5;In a box;50");
+		}
+
+		TEST_METHOD(TransportInputOperator)
+		{
+			std::istringstream iss("25.5 In a box 50");
+			Transport transport;
+			iss >> transport;
+			Assert::AreEqual(transport.getTemperature(), Transport::Temperature(25.5));
+			Assert::AreEqual(transport.getPackaging(), Transport::Packaging("In"));
+			Assert::AreEqual(transport.getHumidity(), Transport::Humidity(50));
+		}
+
+		TEST_METHOD(WeightAndVolumeInputOperator_ValidInput)
+		{
+			std::istringstream iss("10.5;20.3");
+			weightAndVolume wv;
+			iss >> wv;
+			Assert::AreEqual(wv.getWeight(), weightAndVolume::Weight(10.5));
+			Assert::AreEqual(wv.getVolume(), weightAndVolume::Volume(20.3));
+		}
+
+		TEST_METHOD(WeightAndVolumeInputOperator_InvalidInput)
+		{
+			std::istringstream iss("abc;20.3");
+			weightAndVolume wv;
+			iss >> wv;
+			Assert::IsTrue(iss.fail());
+		}
+
+		TEST_METHOD(WeightAndVolumeInputOperator_NegativeInput)
+		{
+			std::istringstream iss("-10.5;-20.3");
+			weightAndVolume wv;
+			iss >> wv;
+			Assert::AreEqual(wv.getWeight(), weightAndVolume::Weight(-10.5));
+			Assert::AreEqual(wv.getVolume(), weightAndVolume::Volume(-20.3));
+		}
+
+		TEST_METHOD(WeightAndVolumeAssignmentOperator)
+		{
+			weightAndVolume wv1(15.7, 30.9);
+			weightAndVolume wv2;
+			wv2 = wv1;
+			Assert::AreEqual(wv2.getWeight(), weightAndVolume::Weight(15.7));
+			Assert::AreEqual(wv2.getVolume(), weightAndVolume::Volume(30.9));
+		}
+
+		TEST_METHOD(CompareTwoDefaultDates)
+		{
+			Date date1;
+			Date date2;
+			Assert::IsFalse(date1 == date2);
+		}
+
+		TEST_METHOD(DateOutputOperator_ValidDate)
+		{
+			Date date(15, 3, 2023);
+			std::ostringstream oss;
+			oss << date;
+			Assert::AreEqual(oss.str().c_str(), "15/3/2023");
+		}
+
+		TEST_METHOD(DateOutputOperator_DefaultDate)
+		{
+			Date date;
+			std::ostringstream oss;
+			oss << date;
+			Assert::AreEqual(oss.str().c_str(), "N/A");
+		}
+
+		TEST_METHOD(DateInputOperator_ValidInput)
+		{
+			std::istringstream iss("10/12/2022");
+			Date date;
+			iss >> date;
+			Assert::AreEqual(date.getDay(), 10);
+			Assert::AreEqual(date.getMonthDigits(), 12);
+			Assert::AreEqual(date.getYear(), 2022);
+		}
+
+		TEST_METHOD(DateInputOperator_DefaultInput)
+		{
+			std::istringstream iss("N/A");
+			Date date;
+			iss >> date;
+			Assert::AreEqual(date.getYear(), 1);
 		}
 	};
 }
