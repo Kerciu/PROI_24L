5b83621ee4b5528a7de0b567b98914b582f1d859
Kerciu <kerciuuu@gmail.com>
2024-04-19 19:45:16 +0200

    added getLastItem and polymorphic constructors in collection class

--- Zadanie4/Figures/Collection.cpp
+++ Zadanie4/Figures/Collection.cpp
@@ -1,1 +1,25 @@
-//#include "Collection.h"
+#include <exception>
+#include "Figure.h"
+#include "Collection.h"
+#include <stdexcept>
+
+Collection::Collection(std::unique_ptr<Figure> itemToCollect)
+{
+	pointerCollection.push_back(std::move(itemToCollect));
+}
+
+Collection::Collection(std::vector<std::unique_ptr<Figure>> pointerCollection) : pointerCollection(std::move(pointerCollection)) { }
+
+inline void Collection::addItem(std::unique_ptr<Figure> item)
+{
+	pointerCollection.push_back(std::move(item));
+}
+
+inline std::unique_ptr<Figure> Collection::getLastItem()
+{
+	size_t collectionSize = pointerCollection.size();
+	if (collectionSize > 0) return std::move(pointerCollection[collectionSize - 1]);
+	else throw std::out_of_range("Insufficient elements in the collecton");
+}
+
+
--- Zadanie4/Figures/Collection.h
+++ Zadanie4/Figures/Collection.h
@@ -1,15 +1,17 @@
-//#pragma once
-//#include <vector>
-//#include <memory>
-//#include "Figure.h"
-//
-//class Collection
-//{
-//private:
-//	std::vector<std::unique_ptr<Figure>> pointerCollection;
-//
-//	void createPointerIfAdded(const Figure& fig) {
-//		auto ptr = std::make_unique<Figure>(fig);
-//		pointerCollection.push_back(std::move(ptr));
-//	}
-//};
+#pragma once
+#include <vector>
+#include <memory>
+#include "Figure.h"
+
+class Collection
+{
+private:
+	std::vector<std::unique_ptr<Figure>> pointerCollection;
+
+public:
+	Collection(std::unique_ptr<Figure> itemToCollect);
+	Collection(std::vector<std::unique_ptr<Figure>> pointerCollection);
+
+	inline void addItem(std::unique_ptr<Figure> item);
+	inline std::unique_ptr<Figure> getLastItem();
+};
