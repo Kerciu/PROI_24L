982b9660b4cffdddbd0ee42151a9d45739e5829b
Kerciu <kerciuuu@gmail.com>
2024-04-20 11:45:14 +0200

    added more container methods, need to fix bugs for it to even compile

--- Zadanie4/Figures/Collection.cpp
+++ Zadanie4/Figures/Collection.cpp
@@ -3,12 +3,28 @@
 #include "Collection.h"
 #include <stdexcept>
 
+Collection::Collection() : pointerCollection() { }
+
 Collection::Collection(std::unique_ptr<Figure> itemToCollect)
 {
 	pointerCollection.push_back(std::move(itemToCollect));
 }
 
 Collection::Collection(std::vector<std::unique_ptr<Figure>> pointerCollection) : pointerCollection(std::move(pointerCollection)) { }
+
+inline void Collection::deleteItem(std::unique_ptr<Figure> item)
+{
+}
+
+inline bool Collection::findItem(std::unique_ptr<Figure> item) const
+{
+	return false;
+}
+
+inline size_t Collection::collectionSize() const
+{
+	return size_t();
+}
 
 inline void Collection::addItem(std::unique_ptr<Figure> item)
 {
@@ -22,4 +38,26 @@ inline std::unique_ptr<Figure> Collection::getLastItem()
 	else throw std::out_of_range("Insufficient elements in the collecton");
 }
 
+inline void Collection::operator+(const Collection& other) const
+{
+}
 
+inline void Collection::operator=(const Collection& other) const
+{
+}
+
+inline void Collection::operator==(const Collection& other) const
+{
+}
+
+inline std::ostream& operator<<(std::ostream& os, const Collection& collect)
+{
+	os << "Hello World!\n";
+	return os;
+}
+
+inline std::istream& operator>>(std::istream& is, const Collection& collect)
+{
+	is >> collect;
+	return is;
+}
--- Zadanie4/UnitTests/UnitTests.cpp
+++ Zadanie4/UnitTests/UnitTests.cpp
@@ -65,5 +65,65 @@ namespace UnitTests
 			Assert::IsTrue(col2.getLastItem() != nullptr);
 			Assert::AreEqual("red", col2.getLastItem()->getFill().c_str());
 		}
+
+		TEST_METHOD(AddToCollection)
+		{
+			std::unique_ptr<Circle> circle1 = std::make_unique<Circle>("brown", "yellow");
+			std::unique_ptr<Circle> circle2 = std::make_unique<Circle>("brown", "green");
+			std::unique_ptr<Circle> circle3 = std::make_unique<Circle>("brown", "black");
+			Collection collection;
+
+			collection.addItem(std::move(circle1));
+			collection.addItem(std::move(circle2));
+			Assert::IsTrue(collection.findItem(std::move(circle1)) == true);
+			Assert::IsTrue(collection.findItem(std::move(circle3)) == false);
+			Assert::AreEqual(collection.getLastItem()->getFill().c_str(), "brown");
+		}
+
+		TEST_METHOD(DeleteFromCollection)
+		{
+			std::unique_ptr<Circle> circle1 = std::make_unique<Circle>("brown", "yellow");
+			std::unique_ptr<Circle> circle2 = std::make_unique<Circle>("brown", "green");
+			std::unique_ptr<Circle> circle3 = std::make_unique<Circle>("brown", "black");
+			Collection collection;
+
+			collection.addItem(std::move(circle1));
+			collection.addItem(std::move(circle2));
+			collection.addItem(std::move(circle3));
+			Assert::IsTrue(collection.findItem(std::move(circle1)) == true);
+			Assert::IsTrue(collection.findItem(std::move(circle2)) == true);
+			Assert::IsTrue(collection.findItem(std::move(circle3)) == true);
+			collection.deleteItem(std::move(circle3));
+			Assert::IsTrue(collection.findItem(std::move(circle3)) == false);
+		}
+
+		TEST_METHOD(AddTwoCollections)
+		{
+			std::unique_ptr<Circle> circle1 = std::make_unique<Circle>("brown", "yellow");
+			std::unique_ptr<Circle> circle2 = std::make_unique<Circle>("brown", "green");
+			std::unique_ptr<Circle> circle3 = std::make_unique<Circle>("brown", "black");
+			Collection col1;
+			Collection col2;
+			col1.addItem(std::move(circle1));
+			col1.addItem(std::move(circle2));
+			col2.addItem(std::move(circle3));
+			Collection col3 = col1 + col2;
+			Assert::AreEqual(col3.collectionSize(), 3);
+		}
+
+		TEST_METHOD(AssignCollection)
+		{
+			std::unique_ptr<Circle> circle1 = std::make_unique<Circle>("brown", "yellow");
+			std::unique_ptr<Circle> circle2 = std::make_unique<Circle>("brown", "green");
+			std::unique_ptr<Circle> circle3 = std::make_unique<Circle>("brown", "black");
+			Collection col1;
+			Collection col2;
+			col1.addItem(std::move(circle1));
+			col1.addItem(std::move(circle2));
+			Assert::AreEqual(col1.collectionSize(), 2);
+			col2.addItem(std::move(circle3));
+			col1 = col2;
+			Assert::AreEqual(col1.collectionSize(), 1);
+		}
 	};
 }
