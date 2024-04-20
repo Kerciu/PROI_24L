3d33e8382b20845eca0ca954825c1b8390c2ffbc
Kerciu <kerciuuu@gmail.com>
2024-04-20 19:28:12 +0200

    added collection functionalities

--- Zadanie4/Figures/Collection.cpp
+++ Zadanie4/Figures/Collection.cpp
@@ -12,52 +12,96 @@ Collection::Collection(std::unique_ptr<Figure> itemToCollect)
 
 Collection::Collection(std::vector<std::unique_ptr<Figure>> pointerCollection) : pointerCollection(std::move(pointerCollection)) { }
 
-inline void Collection::deleteItem(std::unique_ptr<Figure> item)
+Collection::Collection(const Collection& other)
 {
+	pointerCollection.reserve(other.pointerCollection.size());
+	for (const auto& elem : other.pointerCollection) {
+		pointerCollection.push_back(std::make_unique<Figure>(*elem));
+	}
 }
 
-inline bool Collection::findItem(std::unique_ptr<Figure> item) const
+Collection::~Collection() { }
+
+void Collection::deleteItem(std::unique_ptr<Figure> item)
 {
-	return false;
+	auto it = std::find(pointerCollection.begin(), pointerCollection.end(), item);
+	if (it != pointerCollection.end()) {
+		pointerCollection.erase(it);
+	}
+	else {
+		throw std::out_of_range("No such element found in the collection");
+	}
 }
 
-inline size_t Collection::collectionSize() const
+bool Collection::findItem(std::unique_ptr<Figure> item) const
 {
-	return size_t();
+	return (std::find(pointerCollection.begin(), pointerCollection.end(), item) != pointerCollection.end());
 }
 
-inline void Collection::addItem(std::unique_ptr<Figure> item)
+std::unique_ptr<Figure> Collection::popItem()
 {
-	pointerCollection.push_back(std::move(item));
+	if (collectionSize() != 0) {
+		std::unique_ptr<Figure> result = std::move(pointerCollection.back());
+		pointerCollection.pop_back();
+		return result;
+	}
+	else throw std::out_of_range("The collection is empty");
 }
 
-inline std::unique_ptr<Figure> Collection::getLastItem()
+size_t Collection::collectionSize() const
+{
+	return pointerCollection.size();
+}
+
+void Collection::addItem(std::unique_ptr<Figure> item)
+{
+	auto it = std::find(pointerCollection.begin(), pointerCollection.end(), item);
+	if (it == pointerCollection.end()) {
+		pointerCollection.push_back(std::move(item));
+	}
+	else {
+		throw std::out_of_range("Element already in collection");
+	}
+}
+
+std::unique_ptr<Figure> Collection::getLastItem()
 {
 	size_t collectionSize = pointerCollection.size();
 	if (collectionSize > 0) return std::move(pointerCollection[collectionSize - 1]);
 	else throw std::out_of_range("Insufficient elements in the collecton");
 }
 
-inline void Collection::operator+(const Collection& other) const
+std::string Collection::generateSVGString()
 {
+	return std::string();
 }
 
-inline void Collection::operator=(const Collection& other) const
+Collection& Collection::operator+(Collection& other) const
 {
+	Collection result(*this);
+
+	for (size_t i = 0; i < other.collectionSize(); ++i) {
+		std::unique_ptr<Figure> item = std::move(other.pointerCollection[i]);
+		if (!result.findItem(std::move(item))) {
+			result.addItem(std::move(item));
+		}
+	}
+	return result;
 }
 
-inline void Collection::operator==(const Collection& other) const
+Collection& Collection::operator=(Collection& other)
 {
+	if (&other != this) {
+		pointerCollection = std::move(other.pointerCollection);
+	}
+	return *this;
 }
 
-inline std::ostream& operator<<(std::ostream& os, const Collection& collect)
+bool Collection::operator==(const Collection& other) const
 {
-	os << "Hello World!\n";
-	return os;
-}
-
-inline std::istream& operator>>(std::istream& is, const Collection& collect)
-{
-	is >> collect;
-	return is;
+	if (collectionSize() != other.collectionSize()) return false;
+	for (size_t i = 0; i < collectionSize(); ++i) {
+		if (!(*pointerCollection[i] == *other.pointerCollection[i])) return false;
+	}
+	return true;
 }
--- Zadanie4/Figures/Collection.h
+++ Zadanie4/Figures/Collection.h
@@ -9,9 +9,21 @@ private:
 	std::vector<std::unique_ptr<Figure>> pointerCollection;
 
 public:
+	Collection();
 	Collection(std::unique_ptr<Figure> itemToCollect);
 	Collection(std::vector<std::unique_ptr<Figure>> pointerCollection);
+	Collection(const Collection& other);
+	~Collection();
 
-	inline void addItem(std::unique_ptr<Figure> item);
-	inline std::unique_ptr<Figure> getLastItem();
+	void addItem(std::unique_ptr<Figure> item);
+	void deleteItem(std::unique_ptr<Figure> item);
+	bool findItem(std::unique_ptr<Figure> item) const;
+	std::unique_ptr<Figure> popItem();
+	size_t collectionSize() const;
+	std::unique_ptr<Figure> getLastItem();
+	std::string generateSVGString();
+
+	Collection& operator+(Collection& other) const;
+	Collection& operator=(Collection& other);
+	bool operator==(const Collection& other) const;
 };
